5af893b88a6ec88605475dc540709a5adcdd8e76
Hadidomena <106683153+Hadidomena@users.noreply.github.com>
2023-12-15 11:40:37 +0100

    Add files via upload

--- gauss.c
+++ gauss.c
@@ -8,7 +8,21 @@ int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
 		 */
+	for ( int x = 0; x < mat->r; x++ ) {
 
-		return 0;
+		for ( int y = x + 1; y < mat->r; y++ ) {
+			if ( mat->data[y][x] != 0 ) {
+				double wyznacznik = mat->data[x][x] / mat->data[y][x] ;
+				for ( int z = x; z < mat->c; z++ ) {
+					mat->data[y][z] -= mat->data[x][z] / wyznacznik; 	
+				}	
+				b->data[y][0] -= b->data[x][0] / wyznacznik;
+			} else {
+				return 1;
+			}
+		}
+
+	}
+	return 0;
 }
 
