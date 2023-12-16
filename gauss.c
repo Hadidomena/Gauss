eb35b2fc0c6aae41b70101d9ec490d72fe1b9b5e
Hadidomena <106683153+Hadidomena@users.noreply.github.com>
2023-12-16 16:06:07 +0100

    Update gauss.c (#2)

--- gauss.c
+++ gauss.c
@@ -1,21 +1,13 @@
 #include "gauss.h"
-
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
 int eliminate(Matrix *mat, Matrix *b)
 {
-    /**
-     * Tutaj należy umieścić właściwą implemntację.
-     */
-    for (int x = 0; x < mat->r; x++)
+    for ( int x = 0; x < mat->r; x++ )
     {
-        if (mat->data[x][x] != 0)
+        if ( mat->data[x][x] != 0 ) // jesli element na diagonali jest niezerowy mozemy dokonac normalnego zerowania kolumny
         {
-            for (int y = x + 1; y < mat->r; y++)
+            for ( int y = x + 1; y < mat->r; y++ )
             {
-                if (mat->data[y][x] != 0)
+                if ( mat->data[y][x] != 0 )
                 {
                     double wyznacznik = mat->data[x][x] / mat->data[y][x];
                     for (int z = x; z < mat->c; z++)
@@ -24,25 +16,27 @@ int eliminate(Matrix *mat, Matrix *b)
                     }
                     b->data[y][0] -= b->data[x][0] / wyznacznik;
                 }
-                else
-                {
-                }
+                else {} // jesli element w zerowanej kolumnie jest juz rowny 0 pomijamy ja
             }
         }
-        else
+        else 
         {
-			double *tmp1 = mat->data[x];
-			double *tmp2 = b->data[x];
-			int z = x;
-			while ( mat->data[z][x] == 0 && z < mat->r ) { z++; }
-			if ( z != mat->r ) {
-				mat->data[x] = mat->data[z];
-				b->data[x] = b->data[z];
-				mat->data[z] = tmp1;
-				b->data[z] = tmp2;
-			}
-			x--;
+		double *tmp1 = mat->data[x]; 
+		double *tmp2 = b->data[x];
+		int z = x;
+		while ( mat->data[z][x] == 0 && z < mat->r ) { z++; }
+		
+		if ( z != mat->r ) 	
+		{
+			mat->data[x] = mat->data[z];
+			b->data[x] = b->data[z];
+			mat->data[z] = tmp1;
+			b->data[z] = tmp2;
+		}
+		
+		x--;
         }
     }
+	
     return 0;
 }
