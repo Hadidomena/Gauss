30543cd9d6b9d4091e179a16b06376adc94209b0
Lukasz004 <153275622+Lukasz004@users.noreply.github.com>
2023-12-22 10:22:03 +0100

    Add files via upload
    Drobne poprawki w działaniu funkcji

--- backsubst.c
+++ backsubst.c
@@ -4,24 +4,29 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-	int k; //iteracja po kolumnach
-	int w; //iteracja po wierszach
-	if(sizeof(mat->data[0])/sizeof(mat->data[0][0]) != x->r) //sprawdzenie rozmiarow macierzy
-		return 2;
-	for(w=(x->r)-1; w>=0;w--)
-	{
-		if(mat->data[w][w] != 0) //sprawdzenie czy element na diagonali nie jest rowny 0
-		{
-			x->data[w][0] = b->data[w][0]/mat->data[w][w];
-			for(k=(x->r)-1;k>w; k--)
-				x->data[w][0]-= x->data[k][0]*mat->data[w][k]/mat->data[w][w];
-		}
-		else
-			return 1;
-	}
+int backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+    int k; // iteracja po kolumnach
+    int w; // iteracja po wierszach
+    if (mat->c != mat->r || mat->r != b->r)
+    {
+        return 2;
+    }
+    for (w = x->r - 1; w >= 0; w--)
+    {
+        if (mat->data[w][w] != 0) // sprawdzenie czy element na diagonali nie jest rowny 0
+        {
+            x->data[w][0] = b->data[w][0] / mat->data[w][w];
+            for (k = (x->r) - 1; k > w; k--)
+            {
+                x->data[w][0] -= x->data[k][0] * mat->data[w][k] / mat->data[w][w];
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
 
-	return 0;
+    return 0;
 }
-
-
